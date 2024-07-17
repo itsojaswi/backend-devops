7f3b98fff4fc76b4fbd2e222ca0c1c866bf2b154
Your Name <you@example.com>
2024-07-17 21:38:56 +0545

    push

--- server.js
+++ server.js
@@ -14,7 +14,11 @@ app.use(express.json());
 
 // Middleware to enable CORS (Cross-Origin Resource Sharing)
 // Adjust origin as needed based on your frontend setup
-app.use(cors());
+
+app.use(cors({}));
+
+// Specify a port number for the server
+const port = process.env.PORT || 4000;
 
 // Middleware to log requests (optional)
 app.use((req, res, next) => {
@@ -31,9 +35,6 @@ app.get("/", (req, res) => {
 app.use("/api/workouts", workoutRoutes);
 app.use("/api/user", userRoutes);
 
-// Specify a port number for the server
-const port = process.env.PORT || 4000;
-
 // Connect to MongoDB
 mongoose
   .connect(process.env.MONGO_URI, {
@@ -41,8 +42,8 @@ mongoose
     useUnifiedTopology: true,
   })
   .then(() => {
-    // Start the server and listen to the specified port with HTTPS
-    http.createServer(httpsOptions, app).listen(port, () => {
+    // Start the server and listen to the specified port
+    app.listen(port, () => {
       console.log(
         `Connected to the database. Server is running on port ${port}.`
       );
