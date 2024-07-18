52c713680a024971ebe0128e14670b2d61dbbf0f
Your Name <you@example.com>
2024-07-18 11:14:59 +0545

    authentication backend

--- models/userModel.js
+++ models/userModel.js
@@ -1,4 +1,6 @@
 const mongoose = require("mongoose");
+const bycrypt = require("bcrypt");
+const validator = require("validator");
 
 const Schema = mongoose.Schema; // Correct spelling of Schema
 
@@ -13,5 +15,55 @@ const userSchema = new Schema({
     required: true,
   },
 });
+
+//  static signup method
+userSchema.statics.signup = async function (email, password) {
+  // validation
+  if (!email || !password) {
+    throw Error("All fields must be filled");
+  }
+
+  if (!validator.isEmail(email)) {
+    throw Error("Email is not valid");
+  }
+
+  if (!validator.isStrongPassword(password)) {
+    throw Error("Password not strong enough");
+  }
+
+  const exists = await this.findOne({ email });
+
+  if (exists) {
+    throw Error("Email already in use.");
+  }
+
+  const salt = await bycrypt.genSalt(10);
+  const hash = await bycrypt.hash(password, salt);
+
+  const user = await this.create({ email, password: hash });
+
+  return user;
+};
+
+// static login method
+userSchema.statics.login = async function (email, password) {
+  if (!email || !password) {
+    throw Error("All fields must be filled");
+  }
+
+  const user = await this.findOne({ email });
+
+  if (!user) {
+    throw Error("Incorrect email.");
+  }
+
+  const match = await bycrypt.compare(password, user.password);
+
+  if (!match) {
+    throw Error("Incorrect password");
+  }
+
+  return user;
+};
 
 module.exports = mongoose.model("User", userSchema);
